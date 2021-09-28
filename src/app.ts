7bdcdeacfadbb849c610eb18965056c9fbf43dea
Marie Ow <marieow35@gmail.com>
2021-09-28 16:05:03 -0400

    octopus

--- src/app.ts
+++ src/app.ts
@@ -14,22 +14,146 @@ class Drawing extends DrawingCommon {
 	*/
 	initializeScene(){
         const objectRoot = new THREE.Group();
+        const head = new THREE.Group();
+        const tentacles = new THREE.Group();
+        const plant = new THREE.Group();
 
-        var geometry: THREE.BufferGeometry = new THREE.CylinderGeometry( 0, 1, 3, 10, 1 );
-        var material = new THREE.MeshPhongMaterial( { color: 0x00ffff, flatShading: true } );
-        var mesh = new THREE.Mesh( geometry, material );
-
+        var material = new THREE.MeshToonMaterial( { color: 0xd64864} );
+        //octopus head
+        var radius =  7.4;  
+        var widthSegments = 12;  
+        var heightSegments = 8;  
+        var geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        var mesh = new THREE.Mesh( geometry, material);
         mesh.position.set(1,0,0);
-        objectRoot.add( mesh );
+        head.add( mesh );
 
-        geometry = new THREE.TorusGeometry( 1, 0.2, 30, 40 );
-        material = new THREE.MeshPhongMaterial( { color: 0xffff00, flatShading: true } );
-        mesh = new THREE.Mesh( geometry, material );
+        //first eye
+        var eyeMaterial = new THREE.MeshToonMaterial( { color: 0x030303} );
+        radius = 1;  
+        var detail = 3;  
+        const geometryEye = new THREE.DodecahedronGeometry(radius, detail);
+        var meshEye = new THREE.Mesh(geometryEye, eyeMaterial);
+        meshEye.position.set(-2, 1, 6);
+        head.add(meshEye);
 
-        mesh.position.set(-1,0,0);
-        objectRoot.add( mesh );
 
-        this.scene.add( objectRoot );
+        //second eye
+        var meshEye = new THREE.Mesh(geometryEye, eyeMaterial);
+        meshEye.position.set(4, 1, 6);
+        head.add(meshEye);
+
+        //nose
+        radius =  1;  
+        const tubeRadius =  1;  
+        radialSegments = 26;  
+        const tubularSegments = 48;  
+        const geometryNose = new THREE.TorusGeometry(radius, tubeRadius, radialSegments, tubularSegments);
+        var meshNose = new THREE.Mesh(geometryNose, material);
+        meshNose.position.set(1,0,7);
+        head.add(meshNose);
+
+        //inner nose
+        const geometryInner = new THREE.DodecahedronGeometry(radius, detail);
+        var meshInner = new THREE.Mesh(geometryEye, eyeMaterial);
+        meshInner.position.set(1, 0, 7.1);
+        head.add(meshInner);
+       
+        //octopus tentacles 
+        // tentacle 1    
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(7, -6,-1);
+        tentacles.add(mesh);
+    
+        //tentacle 2
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(-5.5,-5,1);
+        tentacles.add( mesh );
+
+        //tentacle 3
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(-4,-6, -3);
+        tentacles.add( mesh );
+
+        //tentacle 4
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(4,-6, -5);
+        tentacles.add( mesh );
+
+        //tentacle 5
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(-0.5,-6, -5);
+        tentacles.add( mesh );
+
+         //tentacle 6
+         radius =  1.7;  
+         widthSegments = 12;  
+         heightSegments = 8;  
+         geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+         mesh = new THREE.Mesh( geometry, material);
+         mesh.position.set(-3, -6, 4);
+         tentacles.add( mesh );
+ 
+        //  tentacle 7
+         radius =  1.7;  
+         widthSegments = 12;  
+         heightSegments = 8;  
+         geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+         mesh = new THREE.Mesh( geometry, material);
+         mesh.position.set(5, -7, 3);
+         tentacles.add( mesh );
+
+        //tentacle 8
+        radius =  1.7;  
+        widthSegments = 12;  
+        heightSegments = 8;  
+        geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        mesh = new THREE.Mesh( geometry, material);
+        mesh.position.set(1,-7, 5);
+        tentacles.add( mesh );
+
+        //octopus plant
+        var stemColor = new THREE.MeshToonMaterial( { color: 0x1ba942} );
+        var radiusTop =  0.6;  
+        var radiusBottom =  0.6;  
+        var height = 3.6;  
+        var radialSegments = 12;  
+        var geometryPlant = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+        var meshPlant = new THREE.Mesh( geometryPlant, stemColor);
+        meshPlant.position.set(0, 8.5, 1);
+        plant.add(meshPlant);
+
+        //petal
+        const petalColor = new THREE.MeshToonMaterial( { color: 0xf7c313} );
+        radius = 2;  
+        const geometryPetal = new THREE.OctahedronGeometry(radius);
+        var meshPetal = new THREE.Mesh( geometryPetal, petalColor);
+        meshPetal.position.set(0, 10, 1);
+        plant.add(meshPetal);
+        
+        objectRoot.add(head, tentacles, plant);
+
+        this.scene.add(objectRoot);
     }
 
 	/*
